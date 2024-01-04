529e15d2ec3416c692e5513e88dcacc413cfe964
rashedrk <rashedrk001@gmail.com>
2024-01-04 09:37:40 +0600

    mobile responseive search section

--- src/components/SearchTutor/SearchTutor.js
+++ src/components/SearchTutor/SearchTutor.js
@@ -3,14 +3,14 @@ import React from 'react';
 const SearchTutor = () => {
     return (
         <div className='bg-no-repeat bg-cover' style={{ backgroundImage: 'url(https://i.ibb.co/R4kczMf/pexels-andrea-piacquadio-3769995.jpg)', }}>
-            <div className='bg-[#0d2c3d] bg-opacity-60 min-h-screen flex items-center justify-center gap-12 w-full'>
-                <div className='text-white drop-shadow-lg'>
+            <div className='bg-[#0d2c3d] bg-opacity-60 min-h-screen md:flex items-center justify-center gap-12 w-full p-3'>
+                <div className='text-white drop-shadow-lg mb-5 md:mb-0'>
                     <h1 className='font-bold text-6xl'>Find The</h1>
                     <h2 className='font-bold text-5xl my-6 border-b-2 border-b-orange-400'>Perfect Tutor for</h2>
                     <h1 className='font-bold text-6xl'>Offline & Online</h1>
                 </div>
-                <div className='w-[45%] p-9 bg-[#0a202c] bg-opacity-50 rounded-xl'>
-                    <div className='grid grid-cols-2 gap-5 mb-4'>
+                <div className='md:w-[45%] p-9 bg-[#0a202c] bg-opacity-50 rounded-xl'>
+                    <div className='grid md:grid-cols-2 gap-5 mb-4'>
                         <select className="select select-bordered w-full max-w-xs rounded-full">
                             <option disabled selected>Select District</option>
                             <option>Dhaka</option>
