70dba8d02f25720dc70644f92e3fa8a147924595
rashedrk <rashedrk001@gmail.com>
2024-07-20 17:00:15 +0600

    tutors current tuition

--- src/app/(withDashboardLayout)/dashboard/tutor/current_tuition/page.js
+++ src/app/(withDashboardLayout)/dashboard/tutor/current_tuition/page.js
@@ -1,11 +1,76 @@
-import CurrentTuition from "@/components/CurrentTuition/CurrentTuition";
+'use client'
 
-const CurrentTuitionPage = () => {
+import DataTable from "@/components/shared/DataTable/DataTable";
+import { useGetCurrentTuitionsQuery } from "@/redux/features/tuition/tuitionApi";
+
+const TutorCurrentTuitionPage = () => {
+    const { data, isLoading } = useGetCurrentTuitionsQuery(undefined);
+    console.log(data);
+
+    const columns = [
+        {
+            name: 'Subject',
+            row: "subject",
+        },
+        {
+            name: 'Class',
+            row: "class"
+        },
+        {
+            name: 'Student Name',
+            row: "student.name",
+        },
+        {
+            name: 'Contact No',
+            row: "student.contactNo",
+        },
+        {
+            name: 'Email',
+            row: "student.email",
+        },
+        {
+            name: 'Duration',
+            row: (rowData) => `${rowData.schedule.startTime} - ${rowData.schedule.endTime}`,
+        },
+        {
+            name: 'Days',
+            row: (rowData) => rowData.schedule.days.join(', ')
+        },
+        {
+            name: 'Address',
+            row: (rowData) => `${rowData.address.address}, ${rowData.address.area}, ${rowData.address.district}`
+        },
+        // {
+        //     name: 'Action',
+        //     row: (rowData) => <div className="dropdown dropdown-end">
+        //         <div tabIndex={0} role="button" className="btn bg-transparent hover:bg-transparent m-1"><SlOptionsVertical /></div>
+        //         <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box shadow-2xl z-[1] w-44 p-2 ">
+        //             {
+        //                 // rowData.status === 'booked' ?
+        //                 //     <>
+        //                 //     <li><Link href={`/tutor/${rowData.selected_tutor}`}><FaRegEye /> Tutor Details</Link></li>
+        //                 //     </> :
+        //                 //     <>
+        //                 //         <li><Link href={""}><RiEdit2Line fontSize={"20px"} /> Edit</Link></li>
+        //                 //         <li><Link href={`/dashboard/student/posted_tuitions/${rowData.tuition_id}/applied`}><PiUserList fontSize={"20px"} /> Applied Tutors</Link></li>
+        //                 //     </>
+        //             }
+        //         </ul>
+        //     </div>
+        // },
+    ];
+
     return (
         <>
-            <CurrentTuition/>
+            {
+                isLoading ? "Loading,,," :
+                    <DataTable
+                        columns={columns}
+                        data={data}
+                    />
+            }
         </>
     );
 };
 
-export default CurrentTuitionPage;
+export default TutorCurrentTuitionPage;
