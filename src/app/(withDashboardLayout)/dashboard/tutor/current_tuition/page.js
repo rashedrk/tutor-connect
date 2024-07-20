fdd92c0231fb694ff6712bac6a2ec161effeaa09
rashedrk <rashedrk001@gmail.com>
2024-07-20 13:07:40 +0600

    current tuition

---
+++ src/app/(withDashboardLayout)/dashboard/tutor/current_tuition/page.js
@@ -0,0 +1,11 @@
+import CurrentTuition from "@/components/CurrentTuition/CurrentTuition";
+
+const CurrentTuitionPage = () => {
+    return (
+        <>
+            <CurrentTuition/>
+        </>
+    );
+};
+
+export default CurrentTuitionPage;
