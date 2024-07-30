30be81a37cdb199ec2b31a31b32e29d4af7db32b
rashedrk <rashedrk001@gmail.com>
2024-07-30 17:11:04 +0600

    all tutors page

--- src/app/(withCommonLayout)/tutors/page.js
+++ src/app/(withCommonLayout)/tutors/page.js
@@ -1,13 +1,11 @@
 // import { Fade } from "react-awesome-reveal";
 import SectionTitle from "@/components/shared/SectionTitle/SectionTitle";
 import TutorCard from "@/components/TutorCard/TutorCard";
-import Link from "next/link";
-import { FaArrowRight } from "react-icons/fa";
 
 
 
 
-const FeaturedTutors = async () => {
+const Tutors = async () => {
 
     const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/tutor`, { cache: "no-store" });
     const tutors = await res.json();
@@ -17,26 +15,21 @@ const FeaturedTutors = async () => {
     return (
         <div className="bg-[#E8E8E8]">
             <SectionTitle
-                titleFirst="Featured"
+                titleFirst="Available"
                 titleLast="Tutors"
             />
             <div className="grid md:grid-cols-3 gap-10 max-w-6xl justify-center pb-10 mx-auto">
-                {/* <Fade triggerOnce direction="left" cascade duration={600} damping={0.2}> */}
                 {
-                    tutors?.data?.slice(0, 6).map(tutor => <TutorCard
+                    tutors?.data?.map(tutor => <TutorCard
                         key={tutor?.tutor_id}
                         tutor={tutor}
                     >
 
                     </TutorCard>)
                 }
-                {/* </Fade> */}
-            </div>
-            <div className="text-center pb-5">
-                <Link href="/tutors" className="secondary-btn btn">See All Tutors <FaArrowRight /></Link>
             </div>
         </div>
     );
 };
 
-export default FeaturedTutors;
+export default Tutors;
