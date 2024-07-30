07921176eb914703a88ab9ef16a11aab5011defa
rashedrk <rashedrk001@gmail.com>
2024-07-31 00:27:03 +0600

    dashboard temporay bg image

--- src/app/(withDashboardLayout)/dashboard/page.js
+++ src/app/(withDashboardLayout)/dashboard/page.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import Image from 'next/image';
+import dashboardBg from '@/assets/svg/dashboard.svg'
 
 const DashboardHome = () => {
     return (
-        <div>
-            This is dashboard
+        <div className='flex justify-center items-center'>
+            <Image src={dashboardBg} alt='Welcome to Dashboard' width={500} height={500} />
         </div>
     );
 };
