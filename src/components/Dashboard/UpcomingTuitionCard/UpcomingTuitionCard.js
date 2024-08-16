dfb36baa8275aa21dbbc666a3a189a1365ea8c5e
rashedrk <rashedrk001@gmail.com>
2024-08-16 16:42:09 +0600

    tutition student dashboard

--- src/components/Dashboard/UpcomingTuitionCard/UpcomingTuitionCard.js
+++ src/components/Dashboard/UpcomingTuitionCard/UpcomingTuitionCard.js
@@ -6,7 +6,7 @@ import { IoLocationOutline } from 'react-icons/io5';
 const UpcomingTuitionCard = ({ tuition }) => {
     return (
         <div className='bg-white shadow rounded-md py-4 px-5 mb-3'>
-            <h6 className='font-semibold text-sm text-[#00A5A7]'>{tuition?.student?.name}</h6>
+            <h6 className='font-semibold text-sm text-[#00A5A7]'>{tuition?.student?.name || tuition?.tutor?.profile?.name || tuition?.selectedTutor?.profile?.name}</h6>
             <p className='text-xs my-1 text-gray-500 flex justify-start items-center gap-1'>
                 <FiClock />{
                     `${dayjs(tuition?.schedule?.startTime).format('h:mm A')} - ${dayjs(tuition?.schedule?.endTime).format('h:mm A')}`
